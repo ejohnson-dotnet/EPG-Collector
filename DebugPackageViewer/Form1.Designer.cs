48c7d1ad8f30e9a7697160985feccca38d9c90b9
Eric Johnson <ejohnson0547@gmail.com>
2021-11-05 13:34:43 -0500

    Upgrading to VS2019 and fixing build issues. Moving configuration files to correct location

---
+++ DebugPackageViewer/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace DebugPackageViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelContents = new System.Windows.Forms.Panel();
+            this.lbFiles = new System.Windows.Forms.CheckedListBox();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.extractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panelContents.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(843, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.extractToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeyDisplayString = "Alt+F4";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // panelContents
+            // 
+            this.panelContents.Controls.Add(this.lbFiles);
+            this.panelContents.Controls.Add(this.tbPath);
+            this.panelContents.Controls.Add(this.label1);
+            this.panelContents.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContents.Location = new System.Drawing.Point(0, 24);
+            this.panelContents.Name = "panelContents";
+            this.panelContents.Size = new System.Drawing.Size(843, 656);
+            this.panelContents.TabIndex = 1;
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(30, 58);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(788, 574);
+            this.lbFiles.TabIndex = 2;
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(93, 16);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(725, 20);
+            this.tbPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path";
+            // 
+            // extractToolStripMenuItem
+            // 
+            this.extractToolStripMenuItem.Name = "extractToolStripMenuItem";
+            this.extractToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.extractToolStripMenuItem.Text = "Extract...";
+            this.extractToolStripMenuItem.Click += new System.EventHandler(this.extractToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 680);
+            this.Controls.Add(this.panelContents);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EPG Collector - Debug Package Viewer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panelContents.ResumeLayout(false);
+            this.panelContents.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Panel panelContents;
+        private System.Windows.Forms.CheckedListBox lbFiles;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem extractToolStripMenuItem;
+    }
+}
+
---
+++ DebugPackageViewer/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Windows.Forms;
+
+using DomainObjects;
+
+namespace DebugPackageViewer
+{
+    public partial class Form1 : Form
+    {
+        private string currentPackageFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Debug Package Files (*.pkg)|*.pkg";
+            openFile.InitialDirectory = Path.Combine(RunParameters.DataDirectory, "Debug Packages");
+            openFile.RestoreDirectory = true;
+            openFile.Title = "EPG Collector - Find Debug Package File";
+
+            DialogResult result = openFile.ShowDialog();
+            if (result == DialogResult.Cancel)
+                return;
+
+            processFile(openFile.FileName);
+        }
+
+        private void processFile(string fileName)
+        {
+            currentPackageFile = fileName;
+            tbPath.Text = fileName;
+
+            lbFiles.Items.Clear();
+
+            BinaryReader reader = new BinaryReader(File.OpenRead(fileName));
+            bool eof = false;
+
+            while (!eof)
+            {
+                int nameLength = getLength(reader, 1);
+                if (nameLength == -1)
+                    eof = true;
+                else
+                {
+                    byte[] nameBytes = getBytes(reader, nameLength);
+                    if (nameBytes == null)
+                        eof = true;
+                    else
+                    {
+                        string name = Encoding.ASCII.GetString(nameBytes);
+
+                        int dataLength = getLength(reader, 4);
+                        if (dataLength == -1)
+                            eof = true;
+                        else
+                        {
+                            byte[] dataBytes = new byte[dataLength];
+                            int bytesRead = reader.Read(dataBytes, 0, dataLength);
+                            if (bytesRead != dataLength)
+                                eof = true;
+
+                            if (dataLength > 1024)
+                            {
+                                byte[] uncompressedBytes = uncompressBytes(dataBytes);
+                                lbFiles.Items.Add(name + " size = " + getSizeDescription(uncompressedBytes.Length));
+                            }
+                            else
+                                lbFiles.Items.Add(name + " size = " + getSizeDescription(dataLength));                            
+                        }
+                    }
+                }
+            }
+
+            reader.Close();
+        }
+
+        private int getLength(BinaryReader reader, int length)
+        {
+            byte[] buffer = new byte[length];
+            int bytesRead = reader.Read(buffer, 0, length);
+            if (bytesRead != length)
+                return -1;
+
+            int result = 0;
+
+            for (int index = length - 1; index > -1; index--)
+                result = (result * 256) + buffer[index];
+
+            return result;
+        }
+
+        private byte[] getBytes(BinaryReader reader, int length)
+        {
+            byte[] buffer = new byte[length];
+            int bytesRead = reader.Read(buffer, 0, length);
+            if (bytesRead != length)
+                return null;
+
+            return buffer;
+        }
+
+        private byte[] uncompressBytes(byte[] buffer)
+        {
+            MemoryStream decompressedStream = new MemoryStream();
+            MemoryStream compressedStream = new MemoryStream(buffer);
+            GZipStream decompressionStream = new GZipStream(compressedStream, CompressionMode.Decompress, false);
+            decompressionStream.CopyTo(decompressedStream);
+            decompressionStream.Flush();
+            
+            decompressedStream.Position = 0;
+            byte[] decompressedBytes = new byte[decompressedStream.Length];
+            decompressedStream.Read(decompressedBytes, 0, decompressedBytes.Length);
+
+            decompressedStream.Close();
+            
+            return decompressedBytes;
+        }
+
+        private string getSizeDescription(int size)
+        {
+            if (size < 1024)
+                return size + " bytes";
+
+            if (size < 1024 * 1024)
+                return (size / 1024).ToString() + "KB";
+
+            return ((double)size / (1024 * 1024)).ToString("0.00") + "MB";            
+        }
+
+        private void extractToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool extractAllFiles = false;
+
+            if (lbFiles.CheckedItems.Count == 0)
+            {
+                DialogResult result = MessageBox.Show("No files selected." + Environment.NewLine + Environment.NewLine +
+                    "Are all files to be extracted?", "EPG Collector - Debug Package Viewer", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Cancel)
+                    return;
+
+                extractAllFiles = result == DialogResult.Yes;
+            }
+
+            FolderBrowserDialog browseFile = new FolderBrowserDialog();
+            browseFile.Description = "EPG Collector - Find Output File Directory";
+            browseFile.SelectedPath = RunParameters.DataDirectory;
+            DialogResult browseResult = browseFile.ShowDialog();
+            if (browseResult == DialogResult.Cancel)
+                return;
+
+            BinaryReader reader = new BinaryReader(File.OpenRead(currentPackageFile));
+            bool eof = false;
+
+            int filesExtracted = 0;
+            int bytesExtracted = 0;
+
+            while (!eof)
+            {
+                int nameLength = getLength(reader, 1);
+                if (nameLength == -1)
+                    eof = true;
+                else
+                {
+                    byte[] nameBytes = getBytes(reader, nameLength);
+                    if (nameBytes == null)
+                        eof = true;
+                    else
+                    {
+                        int dataLength = getLength(reader, 4);
+                        if (dataLength == -1)
+                            eof = true;
+                        else
+                        {
+                            string name = Encoding.ASCII.GetString(nameBytes);
+
+                            if (extractAllFiles || checkNameSelected(name))
+                            {
+                                string fullName = Path.Combine(browseFile.SelectedPath, name);
+                                Logger.Instance.Write("Extracting " + fullName + " data length = " + dataLength);
+
+                                byte[] dataBytes = new byte[dataLength];
+                                int bytesRead = reader.Read(dataBytes, 0, dataLength);
+                                if (bytesRead != dataLength)
+                                    eof = true;
+
+                                if (dataLength > 1024)
+                                {
+                                    byte[] uncompressedBytes = uncompressBytes(dataBytes);
+                                    Logger.Instance.Write("Uncompressed size = " + uncompressedBytes.Length);
+
+                                    FileStream writer = new FileInfo(fullName).OpenWrite();
+                                    writer.Write(uncompressedBytes, 0, uncompressedBytes.Length);
+                                    writer.Close();
+
+                                    bytesExtracted += uncompressedBytes.Length;
+                                }
+                                else
+                                {
+                                    FileStream writer = new FileInfo(fullName).OpenWrite();
+                                    writer.Write(dataBytes, 0, dataBytes.Length);
+                                    writer.Close();
+
+                                    bytesExtracted += dataBytes.Length;
+                                }
+
+                                filesExtracted++;
+                            }
+                            else
+                                reader.BaseStream.Position += dataLength;
+                        }
+                    }
+                }
+            }
+
+            reader.Close();
+
+            MessageBox.Show("Files extracted = " + filesExtracted + Environment.NewLine + Environment.NewLine +
+                "Bytes extracted = " + getSizeDescription(bytesExtracted), "EPG Collector - Debug Package Viewer", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private bool checkNameSelected(string name)
+        {
+            foreach (string listName in lbFiles.CheckedItems)
+            {
+                int index = listName.IndexOf(" size");
+                if (index != -1)
+                {
+                    if (listName.Substring(0, index) == name)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
